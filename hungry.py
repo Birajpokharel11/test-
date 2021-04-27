4d455263f122de0335c37815a9b67d8fa682db16
Biraj Pokharel <buraj.sainik@gmail.com>
2021-04-27 12:04:53 +0545

    First branch changes

--- hungry.py
+++ hungry.py
@@ -2,4 +2,6 @@ hungry=input("are you hungry")
 if hungry =="yes":
   print("eat samosa")
 else:
-  print("do your homework")
+   thirsty=input('are you thirsty'):
+   if thirsty=="yes":
+     print("are you Thirsty")
